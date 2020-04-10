a201a876b053edc6c5f40d99b5076b544c9275be
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2020-04-09 21:04:38 -0300

    Adição dos TextFields para cadastro dos dados do contato.

--- lib/app/pages/contactPage.dart
+++ lib/app/pages/contactPage.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:agendadecontatos/app/models/contact.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +14,9 @@ class ContactPage extends StatefulWidget {
 
 class _ContactPageState extends State<ContactPage> {
   Contact _editedContact;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
 
   @override
   void initState() {
@@ -41,6 +46,51 @@ class _ContactPageState extends State<ContactPage> {
         ),
         onPressed: () {},
       ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: _editedContact.img != null
+                        ? FileImage(File(_editedContact.img))
+                        : AssetImage("assets/images/contact.png"),
+                  ),
+                ),
+              ),
+              onTap: () {},
+            ),
+            generateTextField(nameController, "Nome", TextInputType.text,
+                foco: true),
+            Divider(),
+            generateTextField(
+                emailController, "Email", TextInputType.emailAddress),
+            Divider(),
+            generateTextField(phoneController, "Telefone", TextInputType.phone),
+            Divider(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget generateTextField(
+      TextEditingController controler, String label, TextInputType tipo,
+      {bool foco: false}) {
+    return TextField(
+      controller: controler,
+      keyboardType: tipo,
+      autofocus: foco,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
     );
   }
 }
